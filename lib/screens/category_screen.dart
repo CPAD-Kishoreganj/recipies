3eb2dfb75b8cba3be018419ad471e9ed450a4d97
mhrhabib <mhrhabib39@gmail.com>
2022-01-24 15:27:14 +0600

    initial commit

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Categoryscreen"),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
