4697fa24ab09886d57732585c063cc75c29e359b
mhrhabib <mhrhabib39@gmail.com>
2022-01-30 21:46:02 +0600

    ingredients and steps

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,25 +13,63 @@ class MealDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text(selectedMeal.title),
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 300,
-                child: Image.network(
-                  selectedMeal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
               ),
-              Positioned(
-                top: 200,
-                right: 100,
-                child: Text('Next'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text("Ingrediants"),
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              height: 200,
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                  color: Colors.amber, borderRadius: BorderRadius.circular(20)),
+              child: ListView.builder(
+                itemCount: selectedMeal.ingredients.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(child: Text("${index + 1}")),
+                    title: Text(
+                      selectedMeal.ingredients[index],
+                    ),
+                  );
+                },
               ),
-            ],
-          )
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('Steps'),
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              height: 200,
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                  color: Colors.amber, borderRadius: BorderRadius.circular(20)),
+              child: ListView.builder(
+                itemCount: selectedMeal.steps.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(child: Text("${index + 1}")),
+                    title: Text(
+                      selectedMeal.steps[index],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
